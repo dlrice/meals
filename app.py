8260d249dc6003267d47e80d24c69039357b6b98
Daniel Rice <Daniel.L.Rice@gmail.com>
2022-02-18 21:46:42 +0000

    Use a stylesheet.

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-import yaml
-from jinja2 import Template
 from glob import glob
 import os.path
 import os
+import shutil
+import yaml
+from jinja2 import Template
 
-html_path = "./html"
+build_path = "./build"
 plan_path = "./plan.yaml"
 
 
@@ -25,6 +26,9 @@ def write_template(path, template, d):
 
 
 def main():
+    shutil.rmtree(build_path, ignore_errors=True)
+    os.makedirs(build_path)
+    shutil.copy("./style.css", build_path)
     meal_template = get_template("./templates/meal.html")
     plan_template = get_template("./templates/index.html")
     plan = get_yaml(plan_path)
@@ -35,11 +39,11 @@ def main():
         meal_filename = os.path.split(meal_path)[1]
         meal_name = os.path.splitext(meal_filename)[0]
         meal_html_name = meal_name + ".html"
-        meal_html_path = os.path.join(html_path, meal_html_name)
+        meal_html_path = os.path.join(build_path, meal_html_name)
         meal_data[meal_name] = get_yaml(meal_path)
         write_template(meal_html_path, meal_template, meal_data[meal_name])
 
-    plan_html_path = os.path.join(html_path, "index.html")
+    plan_html_path = os.path.join(build_path, "index.html")
     d = {"meal_data": meal_data, "plan": plan}
     write_template(plan_html_path, plan_template, d)
 
