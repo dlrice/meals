b4b4b9c8f3cfe8a2c2ecb74a1a811a86ad453e27
Daniel Rice <dlrice@ebi.ac.uk>
2023-07-18 20:23:19 +0100

    Build shopping list with jinja template.

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+from collections import defaultdict
 from glob import glob
 import os.path
 import os
+import re
 import shutil
 import yaml
 from jinja2 import Template
@@ -25,6 +27,64 @@ def write_template(path, template, d):
         f.write(template.render(**d))
 
 
+def parse_ingredient(ingredient):
+    p_units = "|".join(
+        [
+            "bag",
+            "bunch",
+            "can",
+            "cups",
+            "cup",
+            "g",
+            "handful",
+            "ml",
+            "square",
+            "stalks",
+            "tbsp",
+            "tin",
+            "tsp",
+        ]
+    )
+    p = re.compile(rf"(?P<quantity>[0-9./]+)\s?(?P<units>{p_units})?(?P<item>.*)")
+    m = p.match(ingredient)
+    if not m:
+        return
+    q = float(m.group("quantity"))
+    u = m.group("units")
+    if u:
+        u = u.strip()
+    i = m.group("item").strip()
+    return q, u, i
+
+
+def get_weekly_items():
+    plan = get_yaml(plan_path)
+    L = []
+    for meal_path in glob("./meals/*.yaml"):
+        meal = get_yaml(meal_path)
+        L += meal["ingredients"]
+    ingredient_to_quantity = defaultdict(float)
+    ingredient_to_unit = {}
+    for el in set(L):
+        q, u, i = parse_ingredient(el)
+        if u:
+            if i in ingredient_to_unit:
+                if ingredient_to_unit[i] != u:
+                    print(f"{i}: {ingredient_to_unit[i]} != {u}")
+                    continue
+            else:
+                ingredient_to_unit[i] = u
+        ingredient_to_quantity[i] += q
+    return [
+        {
+            "quantity": ingredient_to_quantity[i],
+            "unit": ingredient_to_unit.get(i, ""),
+            "name": i,
+        }
+        for i in sorted(ingredient_to_quantity)
+    ]
+
+
 def build():
     shutil.rmtree(build_path, ignore_errors=True)
     os.makedirs(build_path)
@@ -46,6 +106,11 @@ def build():
     plan_html_path = os.path.join(build_path, "index.html")
     d = {"meal_data": meal_data, "plan": plan}
     write_template(plan_html_path, plan_template, d)
+
+    shopping_list_html_path = os.path.join(build_path, "shopping-list.html")
+    shopping_list_template = get_template("./templates/shopping-list.html")
+    d = {"items": get_weekly_items()}
+    write_template(shopping_list_html_path, shopping_list_template, d)
 
 
 if __name__ == "__main__":
